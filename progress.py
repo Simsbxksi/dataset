5da58dc412558337cd96ae83531d1416a3563f36
Simsbxksi <110763234+Simsbxksi@users.noreply.github.com>
2023-05-05 12:06:14 +0200

    Update progress.py
    folder support added

--- progress.py
+++ progress.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python3
-# Needs to be updated to support folders
+# updated to support folders
 # counting all the lines in the csv files
 import os
 num_lines = 0
 for file in os.listdir("."):
+
+    if file.startswith("."):
+        continue
+    if os.path.isdir("./"+file):
+        for f in os.listdir("./"+file):
+            if f.endswith(".csv"):
+                for line in open("./"+file+"/"+f, encoding="utf-8"):
+                    if line.strip()=='':
+                        continue
+                    num_lines+=1
+                num_lines-=2
+
+
     if file.endswith(".csv"):
         for line in open(file, encoding="utf8"):
             if line.strip() == '':
